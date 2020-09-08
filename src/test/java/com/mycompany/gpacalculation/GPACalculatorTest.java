0021c12c6fb9058864d11109b9781b00f0c9523f
Dev-Daljeet <sin10510@sheridancollege.ca>
2020-09-08 12:14:49 -0400

    Added test classes (JUnit 5 - Unit Testing)

---
+++ src/test/java/com/mycompany/gpacalculation/GPACalculatorTest.java
@@ -0,0 +1,78 @@
+package com.mycompany.gpacalculation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GPACalculatorTest {
+
+    private GPACalculator calculator = GPACalculator.getInstance();
+
+    @Test
+    public void gpaShouldBeCalculated() {
+        ArrayList<Course> courseList = new ArrayList<>();
+        String[] gradeArray = {"A+","D","C-"};
+        for (int i=0; i<3; i++)
+        {
+            String name = "Course "+(i+1);
+            int credit = i+2;
+            String grade = gradeArray[i];
+            Course course = new Course(name,credit,grade);
+            courseList.add(course);
+        }
+
+        double result = calculator.calculateGPA(courseList);
+        double expResult = 2.0444444444444443;
+
+        assertEquals(expResult,result);
+    }
+
+    @Test
+    public void exceptionForGradeShouldBEThrown() {
+        ArrayList<Course> courseList = new ArrayList<>();
+        String[] gradeArray = {"X","Y","Z"};       // Invalid Grade
+        for (int i=0; i<3; i++)
+        {
+            String name = "Course "+(i+1);
+            int credit = i+2;
+            String grade = gradeArray[i];
+            Course course = new Course(name,credit,grade);
+            courseList.add(course);
+        }
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+           calculator.calculateGPA(courseList);
+        });
+
+        String result = exception.getMessage();
+        String expResult = "Invalid Grade";
+
+        assertEquals(expResult,result);
+    }
+
+    @Test
+    public void exceptionForCreditShouldBEThrown() {
+        ArrayList<Course> courseList = new ArrayList<>();
+        String[] gradeArray = {"A","D","C-"};
+        for (int i=0; i<3; i++)
+        {
+            String name = "Course "+i;
+            int credit = -i;                                 // Negative or Invalid Credit
+            String grade = gradeArray[i];
+            Course course = new Course(name,credit,grade);
+            courseList.add(course);
+        }
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculator.calculateGPA(courseList);
+        });
+
+        String result = exception.getMessage();
+        String expResult = "Invalid Credit";
+
+        assertEquals(expResult,result);
+    }
+
+}
